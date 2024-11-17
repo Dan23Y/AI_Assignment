59c98eb07b824f436151f18c4cf2f41a5e525779
Dan23Y <DAN.YATES23@bathspa.ac.uk>
2024-11-17 21:34:08 +0000

    I have implemented rules for the dealer to figure out wehter the forward chaining system has worked by getting the best result

--- Task1-Artifical-Intelligence.py
+++ Task1-Artifical-Intelligence.py
@@ -1,27 +1,60 @@
-import pandas as pd
 import random
 
 playing = True
+dealerplay = True
 
 card1 = random.randint(1,10)
 card2 = random.randint(1,10)
 print("Your cards are: ", card1,"and",card2)
 dealercard = random.randint(1,10)
+print("The dealers card is ", dealercard)
 cardTotal = card1 + card2
+print("The total of players cards is" , cardTotal)
 
 while playing:
     if cardTotal >= 17:
-        print("Stand")
+        print("Stand", cardTotal)
         playing = False
-    elif cardTotal >= 11:
+    elif cardTotal <= 11:
         print("hit")
         card3 = random.randint(1,10)
         cardTotal = cardTotal + card3
-    elif 11 >= cardTotal >= 16:
+        print(cardTotal)
+    elif 11 <= cardTotal <= 16:
         if dealercard >= 7:
             print("hit")
             card3 = random.randint(1,10)
             cardTotal = cardTotal + card3
-    elif cardTotal > 21:
+            print(cardTotal)
+        else:
+            print("stand")
+            playing = False
+    elif  cardTotal > 21:
         print("bust")
+        playing = False
 
+dealercard2 = random.randint(1,10)
+dealerTotal = dealercard2 + dealercard
+print("The dealers card is ", dealercard2)
+print("The dealers total is", dealerTotal)
+while dealerplay:
+    if dealerTotal > 21:
+        print("bust")
+        dealerplay = False
+    elif dealerTotal < 17:
+        print("hit")
+        dealercard3 = random.randint(1,10)
+        dealerTotal = dealerTotal + dealercard3
+    elif 17 <= dealerTotal <= 21:
+        print("stand")
+        print(dealerTotal)
+        dealerplay = False
+
+if dealerTotal == 21 and cardTotal == 21:
+    print("The dealer Wins")
+elif cardTotal < 22 and cardTotal > dealerTotal:
+     print("player wins")
+elif dealerTotal < 22 and dealerTotal > cardTotal:
+    print("The dealer Wins")
+elif cardTotal > 21 and dealerTotal > 21:
+    print("both bust")
